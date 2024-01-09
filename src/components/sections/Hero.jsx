e45d9e560dab9e7f6544c1cd9858c71b9bcec6d7
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-01-09 15:22:57 +0100

    Update Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -11,7 +11,19 @@ import { useRef } from "react";
 export default function Hero() {
   const isLoading = useSelector(state=> state.preloader)
   const dispatch = useDispatch()
-  const ref = useRef()
+
+  const parentVariants = {
+    // visible: {
+    //   transition: {
+    //     staggerChildren: 0.5,
+    //   },
+    // },
+  }
+
+  const itemVriants = {
+    hidden: { y: 150, opacity: 0 },
+    visible: { y: 0, opacity: 100 },
+  }
   
 
   return (
@@ -24,17 +36,15 @@ export default function Hero() {
 
       <SocialIcons />
 
-      <div className="font-clash text-center font-medium text-[30px] ssm:text-[34px] xsm:text-[40px] sm:text-[50px] md:text-[60px] mmd:text-[75px]  lg:text-[85px] xl:text-[100px] 2xl:text-[120px]  absolute center w-full -mt-3 xl:-mt-7  ">
+      <motion.div
+        variants={parentVariants}
+        initial='hidden'
+        animate={isLoading ? 'hidden' : 'visible'}
+        className="font-clash text-center font-medium text-[30px] ssm:text-[34px] xsm:text-[40px] sm:text-[50px] md:text-[60px] mmd:text-[75px]  lg:text-[85px] xl:text-[100px] 2xl:text-[120px]  absolute center w-full -mt-3 xl:-mt-7  ">
         <div className="overflow-hidden">
           <motion.div
-            ref={ref}
-            variants={{
-              hidden: { y: 150, opacity: 0 },
-              visible: { y: 0, opacity: 100 },
-            }}
-            initial="hidden"
-            animate={isLoading ? 'hidden' : 'visible'}
             transition={{ duration: 1.2, delay: 1.5, type: "spring" }}
+            variants={itemVriants}
           >
             Abdellah El Idrissi
           </motion.div>
@@ -42,13 +52,8 @@ export default function Hero() {
 
         <div className="-mt-1  xsm:-mt-[6px] sm:-mt-[10px] md:-mt-[12px] mmd:-mt-[18px] lg:-mt-[21px] xl:-mt-[27px] 2xl:-mt-[38px] overflow-hidden ">
           <motion.div
-            variants={{
-              hidden: { y: 150, opacity: 0 },
-              visible: { y: 0, opacity: 100 },
-            }}
-            initial="hidden"
-            animate={isLoading ? 'hidden' : 'visible'}
             transition={{ duration: 1.2, delay: 1.5, type: "spring" }}
+            variants={itemVriants}
           >
             Frontend Developer
           </motion.div>
@@ -56,16 +61,9 @@ export default function Hero() {
 
         <div className="font-general overflow-hidden   text-[14px] ssm:text-[15px] xsm:text-[16px] md:text-[17px] xl:text-[18px] mt-4 mmd:mt-3 font-normal text-black flex items-center justify-center">
           <motion.div
-            variants={{
-              hidden: { y: 100, opacity: 0 },
-              visible: { y: 0, opacity: 100 },
-            }}
-            initial="hidden"
-            animate={isLoading ? 'hidden' : 'visible'}
+            variants={itemVriants}
             transition={{ duration: 1.2, delay:1.5, type: "spring" }}
-            onAnimationComplete={()=> {
-              dispatch(startScroll())
-            }}
+            onAnimationComplete={()=> { dispatch(startScroll()) }}
             className="text"
           >
             Based in Casablanca , Morocco
@@ -79,7 +77,7 @@ export default function Hero() {
             ></Image>
           </motion.div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
